68725d346f9d15f3b3e1e47241de97b3f5305e08
Inception <elkassim321@gmail.com>
2022-03-22 10:12:16 +0100

    add SSL

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,6 +1,8 @@
 apt-get update
 apt-get install -y nginx
 chmod 777 /etc/nginx/nginx.conf
+cp ./tools/default /etc/nginx/sites-available/
+cp ./tools/localhost.crt /root/
+cp ./tools/localhost.key /root/
+cp ./tools/index.php /var/www/html/
 echo "daemon off;" >> /etc/nginx/nginx.conf
-cp ./default /etc/nginx/sites-available/
-
