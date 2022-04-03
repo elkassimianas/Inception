26cfd01a50a02bc74495a19f049d1b33db5c718b
Inception <elkassim321@gmail.com>
2022-04-03 08:17:59 +0000

    error in php-fpm

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,8 +1,12 @@
 apt-get update
+apt-get install -y php7.3-fpm
 apt-get install -y nginx
+apt-get install -y php-mysql php-mbstring php-zip php-gd php-xml php-pear php-gettext php-cgi
+apt-get install  -y php-curl php-intl php-soap php-xmlrpc
 chmod 777 /etc/nginx/nginx.conf
 cp ./tools/default /etc/nginx/sites-available/
 cp ./tools/localhost.crt /root/
 cp ./tools/localhost.key /root/
 cp ./tools/index.php /var/www/html/
 echo "daemon off;" >> /etc/nginx/nginx.conf
+service php7.3-fpm start
