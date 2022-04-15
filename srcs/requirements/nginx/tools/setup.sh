7c21efc3d80cda3640b2139b4ecb863e697e37b1
Inception <elkassim321@gmail.com>
2022-04-15 17:53:25 +0000

    php-fpm is done

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,14 +1,7 @@
-#apt-get update
-#apt-get install -y nginx
-#apt-get install -y php7.3 php7.3-fpm php7.3-curl php7.3-gd
-#apt-get install -y php-mysql php-mbstring php-zip php-gd php-xml php-pear php-gettext php-cgi
-#apt-get install  -y php-curl php-intl php-soap php-xmlrpc
-#chmod 777 /etc/nginx/nginx.conf
-#cp ./tools/default /etc/nginx/sites-available/
-#cp ./tools/localhost.crt /root/
-#cp ./tools/localhost.key /root/
-#cp ./tools/index.php /var/www/html/
-#echo "daemon off;" >> /etc/nginx/nginx.conf
-service php7.3-fpm start
-service nginx start
-service php7.3-fpm status
+apt-get update
+apt-get install -y nginx
+chmod 777 /etc/nginx/nginx.conf
+cp ./tools/default /etc/nginx/sites-available/
+cp ./tools/localhost.crt /root/
+cp ./tools/localhost.key /root/
+echo "daemon off;" >> /etc/nginx/nginx.conf
---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,5 @@
+apt-get update
+apt-get install -y php7.3 php7.3-fpm php-mysql wget
+mkdir -p /var/run/php
+#apt-get install -y php-mysql php-mbstring php-zip php-gd php-xml php-pear php-gettext php-cgi
+#apt-get install  -y php-curl php-intl php-soap php-xmlrpc
