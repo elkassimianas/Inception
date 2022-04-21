d3666088472b15d33fca8d567592239deffa3f05
ael-kass <aelkassim321@gmail.com>
2022-04-21 18:27:15 +0000

    ;

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,8 +1,7 @@
 apt-get update
 apt-get install -y nginx
 chmod 777 /etc/nginx/nginx.conf
-cp ./tools/default /etc/nginx/sites-available/
-cp ./tools/localhost.crt /root/
-cp ./tools/localhost.key /root/
-cp ./tools/index.php /var/www/html/
+cp ./conf/default /etc/nginx/sites-available/
+cp ./conf/localhost.crt /root/
+cp ./conf/localhost.key /root/
 echo "daemon off;" >> /etc/nginx/nginx.conf
